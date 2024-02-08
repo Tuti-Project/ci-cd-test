cf64dd6ef1bef9460317e7a98d11a8bf00a9b7bc
이정우 <acg6138@naver.com>
2024-02-08 18:55:31 +0900

    go

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ROOT_PATH="/home/ubuntu/ci-cd-test"
+ROOT_PATH="/home/ubuntu/test"
 JAR="$ROOT_PATH/application.jar"
 STOP_LOG="$ROOT_PATH/stop.log"
 SERVICE_PID=$(pgrep -f $JAR) # 실행중인 Spring 서버의 PID
