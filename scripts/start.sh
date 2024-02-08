2f9a2906be265da48023bf26e6308bdda28a382d
dlwjddn123 <81271328+dlwjddn123@users.noreply.github.com>
2024-02-08 18:12:57 +0900

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ROOT_PATH="/home/ubuntu/test"
+ROOT_PATH="/home/ubuntu/ci-cd-test"
 JAR="$ROOT_PATH/application.jar"
 
 APP_LOG="$ROOT_PATH/application.log"
@@ -10,7 +10,7 @@ START_LOG="$ROOT_PATH/start.log"
 NOW=$(date +%c)
 
 echo "[$NOW] $JAR 복사" >> $START_LOG
-cp $ROOT_PATH/build/libs/test-1.0.0.jar $JAR
+cp $ROOT_PATH/build/libs/ci-cd-test-1.0.0.jar $JAR
 
 echo "[$NOW] > $JAR 실행" >> $START_LOG
 nohup java -jar $JAR > $APP_LOG 2> $ERROR_LOG &
