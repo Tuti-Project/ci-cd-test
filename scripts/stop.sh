a4147b3e5e0dcdea8aff418cbb36b8ea693967c4
dlwjddn123 <81271328+dlwjddn123@users.noreply.github.com>
2024-02-08 18:13:11 +0900

    Update stop.sh

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ROOT_PATH="/home/ubuntu/test"
+ROOT_PATH="/home/ubuntu/ci-cd-test"
 JAR="$ROOT_PATH/application.jar"
 STOP_LOG="$ROOT_PATH/stop.log"
 SERVICE_PID=$(pgrep -f $JAR) # 실행중인 Spring 서버의 PID
