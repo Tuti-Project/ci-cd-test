9ae0a0e70c9fd77dedaf1912265491087cd15b76
dlwjddn123 <81271328+dlwjddn123@users.noreply.github.com>
2024-02-08 19:30:44 +0900

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,18 +1,24 @@
-#!/usr/bin/env bash
+#!/bin/bash
+BUILD_JAR=$(ls /home/ubuntu/test/build/libs/*.jar)
+JAR_NAME=$(basename $BUILD_JAR)
+echo "> build 파일명: $JAR_NAME" >> /home/ubuntu/test/deploy.log
 
-PROJECT_ROOT="/home/ubuntu/test" #코드가 주입되는 경로
-JAR_FILE="$PROJECT_ROOT/helloworld.jar" #build.gradle에서 설정한 파일명으로 변경
+echo "> build 파일 복사" >> /home/ubuntu/test/deploy.log
+DEPLOY_PATH=/home/ubuntu/test
+cp $BUILD_JAR $DEPLOY_PATH
 
-APP_LOG="$PROJECT_ROOT/application.log"
-ERROR_LOG="$PROJECT_ROOT/error.log"
-DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
+echo "> 현재 실행중인 애플리케이션 pid 확인" >> /home/ubuntu/test/deploy.log
+CURRENT_PID=$(pgrep -f $JAR_NAME)
 
-TIME_NOW=$(date +%c)
+if [ -z $CURRENT_PID ]
+then
+  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /home/ubuntu/test/deploy.log
+else
+  echo "> kill -15 $CURRENT_PID"
+  kill -15 $CURRENT_PID
+  sleep 5
+fi
 
-echo "$TIME_NOW > $JAR_FILE 파일 복사" >> $DEPLOY_LOG
-cp $PROJECT_ROOT/build/libs/*.jar $JAR_FILE
-
-echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
-nohup java -jar $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
-
-CURRENT_PID=$(pgrep -f $JAR_FILE)
+DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
+echo "> DEPLOY_JAR 배포"    >> /home/ubuntu/test/deploy.log
+nohup java -jar $DEPLOY_JAR >> /home/ubuntu/deploy.log 2>/home/ubuntu/test/deploy_err.log &
