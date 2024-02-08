95f27b7b1e46ff2c9971613e73143be820d3881c
dlwjddn123 <81271328+dlwjddn123@users.noreply.github.com>
2024-02-08 19:08:41 +0900

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,19 +1,18 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-ROOT_PATH="/home/ubuntu/test"
-JAR="$ROOT_PATH/application.jar"
+PROJECT_ROOT="/home/ubuntu/test" #코드가 주입되는 경로
+JAR_FILE="$PROJECT_ROOT/helloworld.jar" #build.gradle에서 설정한 파일명으로 변경
 
-APP_LOG="$ROOT_PATH/application.log"
-ERROR_LOG="$ROOT_PATH/error.log"
-START_LOG="$ROOT_PATH/start.log"
+APP_LOG="$PROJECT_ROOT/application.log"
+ERROR_LOG="$PROJECT_ROOT/error.log"
+DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 
-NOW=$(date +%c)
+TIME_NOW=$(date +%c)
 
-echo "[$NOW] $JAR 복사" >> $START_LOG
-cp $ROOT_PATH/build/libs/test-1.0.0.jar $JAR
+echo "$TIME_NOW > $JAR_FILE 파일 복사" >> $DEPLOY_LOG
+cp $PROJECT_ROOT/build/libs/*.jar $JAR_FILE
 
-echo "[$NOW] > $JAR 실행" >> $START_LOG
-nohup java -jar $JAR > $APP_LOG 2> $ERROR_LOG &
+echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
+nohup java -jar $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
 
-SERVICE_PID=$(pgrep -f $JAR)
-echo "[$NOW] > 서비스 PID: $SERVICE_PID" >> $START_LOG
+CURRENT_PID=$(pgrep -f $JAR_FILE)
